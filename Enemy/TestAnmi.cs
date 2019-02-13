1bc2a86bd86955709e2e61091c2e6dafd3fd017a
Zheming Gu <zheminggugu@gmail.com>
2019-02-14 02:14:51 +0800

    Add files via upload

---
+++ Enemy/TestAnmi.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnmi : MonoBehaviour {
+    private Animator anim;
+    private float time = 3f;
+    private float changeTime = 5f;
+    private int cnt = 0;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        time = changeTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+	}
+}
