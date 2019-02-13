3feea078256b38187a0ca86b1cc5c74c83b57e7a
Zheming Gu <zheminggugu@gmail.com>
2019-02-14 02:08:55 +0800

    Add files via upload

---
+++ Enemy/Prefab/orc/A_Data/LearnedBehavior/OrcProvoke.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AI;
+[CreateAssetMenu(menuName = "AI/Enemy/Orc/LearnedBehavior/OrcProvoke")]
+public class OrcProvoke : LearnedBehavior {
+    public string m_Skill;
+
+    public override void Initialize(AICharacterBrain _Brain)
+    {
+        base.Initialize(_Brain);
+        m_brain.m_SensorManager.m_SensorData.m_FinishedDoingCurrentLearnedBehavior = false;
+        _Brain.m_BaseMoveManager.ChangeMoveTarget(_Brain.m_CurrentTransform);
+        m_Animator.SetFloat("Speed", 0);
+        m_Animator.SetTrigger(m_Skill);
+        
+    }
+    public override void PlayLearnedBehavior()
+    {
+        m_brain.m_SensorManager.m_SensorData.m_FinishedDoingCurrentLearnedBehavior = true;
+
+
+    }
+}
